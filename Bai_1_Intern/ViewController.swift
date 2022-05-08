301754408b759727bd92178edfb53d2c35057364
Bảo Long <longvb.ptit@gmail.com>
2022-05-08 11:06:09 +0700

    Initial Commit

---
+++ Bai_1_Intern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bai_1_Intern
+//
+//  Created by Bảo Long on 08/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
