f5afe884d2e7efc9aacd72533d221bac105a6706
longvbptit <longvb.ptit@gmail.com>
2022-06-20 15:39:35 +0700

    Finish

---
+++ Bai_1_Intern/Helpers/Custom/PhoneNumberTextField.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTextField.swift
+//  Bai_1_Intern
+//
+//  Created by Bảo Long on 19/05/2022.
+//
+
+import UIKit
+
+class PhoneNumberTextField: UITextField {
+
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+         if action == #selector(UIResponderStandardEditActions.paste(_:)) {
+             return false
+         }
+         return super.canPerformAction(action, withSender: sender)
+    }
+
+}
